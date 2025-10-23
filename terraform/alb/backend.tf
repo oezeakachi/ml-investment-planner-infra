0bb0ab781f5347648c7eef9100b5d5a47cbade14
oezeakachi <oezeakac@redhat.com>
2025-10-23 23:38:49 +0100

    changed region

--- terraform/alb/backend.tf
+++ terraform/alb/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "ml-inv-planner-one"
+    bucket       = "ml-inv-planner-obi"
     key          = "alb/terraform.tfstate"
     region       = "eu-west-1"
     encrypt      = true
