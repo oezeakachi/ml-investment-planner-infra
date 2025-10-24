7440edec624e65c1e14aac699e750fdee619d0aa
oezeakachi <oezeakac@redhat.com>
2025-10-24 08:01:22 +0100

    changes

--- terraform/eks/variables.tf
+++ terraform/eks/variables.tf
@@ -1,13 +1,13 @@
 variable "aws_region" {
   description = "AWS Region where resources will be created"
   type        = string
-  default     = "eu-west-2"
+  default     = "eu-west-1"
 }
 
 variable "project_name" {
   description = "Prefix name for tagging and naming AWS resources"
   type        = string
-  default     = "ml-investment-planner"
+  default     = "ml-investment-planner-app"
 }
 
 variable "k8s_version" {
